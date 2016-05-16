ca2dacc5eb1e13bf92ebd699752d3a44b839e679
Aman Singh <singh_aman@hotmail.com.au>
2016-05-16 16:06:29 +1000

    insure removed, items select all and deselect all

--- client/main.js
+++ client/main.js
@@ -6,9 +6,26 @@ import './main.html';
 
 
 //Subcription
-Meteor.subscribe('theResultAliexpress');
-Meteor.subscribe('theResultAlibaba');
-Meteor.subscribe('theResultEbay');
+// Meteor.subscribe('theResultAliexpress');
+//  Meteor.subscribe('theResultAlibaba');
+// Meteor.subscribe('theResultEbay');
+Meteor.subscribe('exportItems');
+
+//version of Meteor.subscribe that is stopped when the template is destroyed
+Template.ebayResult.onCreated(function () {
+    //use this.subscribe inside the onCreted callback
+    this.subscribe('theResultEbay');
+});
+
+Template.alibabaResult.onCreated(function () {
+    //use this.subscribe inside the onCreted callback
+    this.subscribe('theResultAlibaba');
+});
+
+Template.aliexpressResult.onCreated(function () {
+    //use this.subscribe inside the onCreted callback
+    this.subscribe('theResultAliexpress');
+});
 
 Template.search.events({
 
@@ -69,6 +86,9 @@ Template.ebayResult.helpers({
     'getEbayResult': function() {
 
         return ResultEbay.find().fetch();
+    },
+    'checked':function() {
+        return this.checked ? "checked":"";
     }
 
 });
@@ -90,6 +110,35 @@ Template.ebayResult.events({
                     }
                 });//removing item here
         }
+    },
+
+    'change [type=checkbox]':function(){
+        var documentID = this._id;
+        var isChecked =  this.checked;
+
+        if(isChecked){
+            //making a sever method call
+            Meteor.call('checkEbayItemFalse', documentID, function(error,res){
+               if(!error){
+                console.log("updated to false");
+            }
+            else {
+                console.log(error);
+            }
+            });
+        }else {
+            //making a server method call 
+
+            Meteor.call('checkEbayItemTrue',documentID, function(error,res){
+                if(!error){
+                console.log("updated to true");
+            }
+        else {
+                console.log(error);
+            }
+        });
+        }
+
     }
 });
 
@@ -110,6 +159,35 @@ Template.alibabaResult.events({
                     }
                 });//removing item here
         }
+    },
+
+    'change [type=checkbox]':function(){
+        var documentID = this._id;
+        var isChecked =  this.checked;
+
+        if(isChecked){
+            //making a sever method call
+            Meteor.call('checkAlibabaItemFalse', documentID, function(error,res){
+                if(!error){
+                    console.log("updated to false");
+                }
+                else {
+                    console.log(error);
+                }
+            });
+        }else {
+            //making a server method call
+
+            Meteor.call('checkAlibabaItemTrue',documentID, function(error,res){
+                if(!error){
+                    console.log("updated to true");
+                }
+                else {
+                    console.log(error);
+                }
+            });
+        }
+
     }
 });
 
@@ -130,17 +208,99 @@ Template.aliexpressResult.events({
                     }
                 });//removing item here
         }
+    },
+    'change [type=checkbox]':function(){
+        var documentID = this._id;
+        var isChecked =  this.checked;
+
+        if(isChecked){
+            //making a sever method call
+            Meteor.call('checkAliexpressItemFalse', documentID, function(error,res){
+                if(!error){
+                    console.log("updated to false");
+                }
+                else {
+                    console.log(error);
+                }
+            });
+        }else {
+            //making a server method call
+
+            Meteor.call('checkAliexpressItemTrue',documentID, function(error,res){
+                if(!error){
+                    console.log("updated to true");
+                }
+                else {
+                    console.log(error);
+                }
+            });
+        }
+
     }
+
 });
 
 Template.alibabaResult.helpers({
     'getAlibabaResult': function() {
         return ResultAlibaba.find();
-    }
+    },
+    'checked':function() {
+        return this.checked ? "checked":"";
+    }                                      
 });
 
 Template.aliexpressResult.helpers({
     'getAliexpressResult': function() {
         return ResultAliexpress.find();
+    },
+    'checked':function() {                 
+        return this.checked ? "checked":"";
+    }                                      
+});
+
+Template.export.events({
+    'click #download': function (e) {
+        //creating a single json object
+        let ebayResult = ResultEbay.find({checked:true},{fields:{productPortal:1,productId:1,productName:1,productPrice:1,productURL:1,_id:0}}).fetch();
+        let alibabaResult = ResultAlibaba.find({checked:true},{fields:{productPortal:1,productId:1,productName:1,productPrice:1,productURL:1,_id:0}}).fetch();
+        let aliexpress = ResultAliexpress.find({checked:true},{fields:{productPortal:1,productId:1,productName:1,productPrice:1,productURL:1,_id:0}}).fetch();
+        let allFinalItems = (ebayResult.concat(alibabaResult)).concat(aliexpress);
+
+        csv = json2csv(allFinalItems, true, true);
+        e.target.href = "data:text/csv;charset=utf-8," + escape(csv)
+        e.target.download = "reportItems.csv";
     }
+});
+
+Template.selectAll.events({
+    'change [type=checkbox]': function (event) {
+
+        //upon selecting all option all items should be selected
+        if($('#selectAll').is(':checked')){
+            Meteor.call('selectAll',function(error,res){
+                if(!error){
+                    console.log("all items all marked now");
+                } 
+                else{
+                    console.log(error);
+                }
+            });
+        }
+        else {
+            console.log("all items are unchecked now");
+            Meteor.call('deselectAll',function(error,res){
+                if(!error){
+                    console.log("all items all unmarked now");
+                }
+                else{
+                    console.log(error);
+                }
+            });
+        }
+
+    }
+});
+
+Accounts.ui.config({
+    passwordSignupFields: "USERNAME_AND_OPTIONAL_EMAIL"
 });
